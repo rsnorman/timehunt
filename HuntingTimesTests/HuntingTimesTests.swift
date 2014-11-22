be5e9ab8e537526d0ca04332eef30f1c6297ed31
rsnorman <rsnorman15@gmail.com>
2014-11-21 22:57:48 -0500

    Initial Commit

---
+++ HuntingTimesTests/HuntingTimesTests.swift
@@ -0,0 +1,36 @@
+//
+//  HuntingTimesTests.swift
+//  HuntingTimesTests
+//
+//  Created by Ryan Norman on 11/21/14.
+//  Copyright (c) 2014 Ryan Norman. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class HuntingTimesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
